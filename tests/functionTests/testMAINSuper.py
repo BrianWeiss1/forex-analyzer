a4976c8c965159d3def03a277f32d7637d9b4acc
Brian Weiss <brianweissct@gmail.com>
2023-08-19 22:24:49 -0400

    Update

--- tests/functionTests/testMAINSuper.py
+++ tests/functionTests/testMAINSuper.py
@@ -1,12 +1,14 @@
-from tests.functionTests.testGrabData import grabHistoricalData
-from tests.functionTests.testSpecial import formatDataset
-from tests.functionTests.testSupertrend import get_supertrend
+from testSpecial import formatDataset
+from testSupertrend import get_supertrend
+from testGrabData import grabHistoricalData
 
 
 if '__main__' == __name__:
     symbol = "EURJPY"
-    while(True):
-        data = grabHistoricalData(symbol)
-        data = formatDataset(data)
-        st, upt, dt = get_supertrend(data['high'], data['low'], data['close'], 9, 3.9)
-        print(st, upt, dt)
+    data = grabHistoricalData(symbol)
+    data = formatDataset(data)
+    st, upt, dt = get_supertrend(data['high'], data['low'], data['close'], 9, 3.9)
+    data['supertrend'] = st
+    data['uptrend'] = upt
+    data['downtrend'] = dt
+    print(data)
